7116a1a95b40bff3b67cc1e65ef7045d249d95da
farhanzaky94 <farhanzaky94@gmail.com>
2024-11-15 23:03:25 +0700

    feat: add function to get day name based on index

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 const apiUrl =
   "https://api.open-meteo.com/v1/forecast?latitude=1.1494&longitude=104.0249&daily=temperature_2m_max,temperature_2m_min&current_weather=true&timezone=auto";
+
+function getDayName(index) {
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const today = new Date();
+  return daysOfWeek[(today.getDay() + index) % 7];
+}
